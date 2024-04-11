e71b4e50c4ff032f00eec4ec770cfb9fe9300436
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2024-04-11 23:24:25 +0800

    more docs

--- src/main/kotlin/top/hellholestudios/xgn/data/DDL.kt
+++ src/main/kotlin/top/hellholestudios/xgn/data/DDL.kt
@@ -17,17 +17,30 @@ data class DDL(
     val visibleGroups: MutableList<String>
 ){
 
+    /**
+     * @return `desc` with `\n` replaced by `<br/>`
+     */
     fun descString():String{
         return desc.replace("<br/>","\n").trim()
     }
+
+    /**
+     * @return `visibleGroups` joined to string
+     */
     fun visibleGroupString():String{
         return visibleGroups.joinToString(",")
     }
 
+    /**
+     * @return `tag` joined to string
+     */
     fun tagString():String{
         return tag.joinToString(", ")
     }
 
+    /**
+     * @return whether `time` is in timeStart~timeEnd inclusive
+     */
     fun contains(time: LocalDateTime):Boolean{
         return time in timeStart..timeEnd
     }
@@ -51,6 +64,9 @@ data class DDL(
         return now.minusDays(90)>=timeEnd
     }
 
+    /**
+     * @return If timeEnd is smaller or equal to timeStart
+     */
     fun isNoDuration():Boolean{
         return timeEnd<=timeStart
     }
@@ -64,7 +80,11 @@ data class DDL(
     }
 
     /**
-     * Not independent.
+     * Frontend function.
+     *
+     * Not independent and uses [DataModel].
+     *
+     * @return user-friendly visibility string
      */
     fun getVisibilityTag():String{
         return when (visibility) {
@@ -79,6 +99,13 @@ data class DDL(
             }
         }
     }
+
+    /**
+     * Frontend Function
+     *
+     * Active means not completed and not finished
+     * @return if the DDL is considered "active" for a certain user
+     */
     fun active(usr: User?):Boolean{
         if(getColor(usr)=="grey"){
             return false
@@ -92,7 +119,9 @@ data class DDL(
         return true
     }
     /**
-     * This gets the color for display using current time
+     * Frontend Function.
+     *
+     * @return the color name for displaying using current time
      */
     fun getColor(user: User?):String{
         if(user!=null && internalID in user.completed){
@@ -108,6 +137,11 @@ data class DDL(
         }
     }
 
+    /**
+     * Frontend Function
+     *
+     * @return the icon to be used
+     */
     fun getIcon(user: User?):String{
         if(user!=null && internalID in user.completed){
             return "check double"
